2f6d1d2e94a599d1e6b39baa33ceabbdd18dd96b
Marcel Felipe <marcel.id22@gmail.com>
2022-11-08 21:56:41 -0300

    BeFake Introduction
    [ADD] BeFake Inicial Project

---
+++ BeFake/BeFake/BeFakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFakeApp.swift
+//  BeFake
+//
+//  Created by Marcel Felipe Gottardi Anesi on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BeFakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
